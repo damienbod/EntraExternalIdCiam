136b90cbaa77ea31418bb876391fd4e336b5014c
damienbod <damien_bod@hotmail.com>
2023-05-27 09:30:42 +0200

    Add a role handler check

--- Ciam/UserAdminHandler.cs
+++ Ciam/UserAdminHandler.cs
@@ -7,7 +7,14 @@ public class UserAdminHandler : AuthorizationHandler<UserAdminRequirement>
     protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context, UserAdminRequirement requirement)
     {
-        
+        var userRole = context.User.FindFirst(c => c.Type == "roles" && c.Value == "user-role");
+        var adminRole = context.User.FindFirst(c => c.Type == "roles" && c.Value == "admin-role");
+
+        if (userRole != null || adminRole != null)
+        {
+            context.Succeed(requirement);
+        }
+
         return Task.CompletedTask;
     }
 }
