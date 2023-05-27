daeab32905efea5c13af54e1194746268e73292a
damienbod <damien_bod@hotmail.com>
2023-05-27 08:31:31 +0200

    Add new pages for show authz

---
+++ Ciam/Pages/User.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ciam.Pages;
+
+public class UserModel : PageModel
+{
+    public List<string> Roles { get; set; } = new List<string>();
+
+    public void OnGet()
+    {
+        var claims = User.Claims.ToList();
+        foreach(var claim in claims)
+        {
+            if(claim != null && claim.Type == "roles")
+            {
+                Roles.Add(claim.Value);
+            }
+        }
+    }
+}
