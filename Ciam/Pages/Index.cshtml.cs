71680c97cd5e1362b37e2388eda2a5cd11178a9a
damienbod <damien_bod@hotmail.com>
2025-04-01 08:59:43 +0200

    Add support for namespaces

--- Ciam/Pages/Index.cshtml.cs
+++ Ciam/Pages/Index.cshtml.cs
@@ -4,14 +4,22 @@ namespace Ciam.Pages;
 
 public class IndexModel : PageModel
 {
-    public List<string> Roles { get; set; } = new List<string>();
+    public List<string> Roles { get; set; } = [];
 
     public void OnGet()
     {
+        // magic namespaces
+        var msMagicNamespace = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+
         var claims = User.Claims.ToList();
         foreach (var claim in claims)
         {
             if (claim != null && claim.Type == "roles")
+            {
+                Roles.Add(claim.Value);
+            }
+
+            if (claim != null && claim.Type == msMagicNamespace)
             {
                 Roles.Add(claim.Value);
             }
--- Ciam/UserAdminHandler.cs
+++ Ciam/UserAdminHandler.cs
@@ -7,8 +7,21 @@ public class UserAdminHandler : AuthorizationHandler<UserAdminRequirement>
     protected override Task HandleRequirementAsync(
         AuthorizationHandlerContext context, UserAdminRequirement requirement)
     {
+        // magic namespaces
+        var msMagicNamespace = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+
         var userRole = context.User.FindFirst(c => c.Type == "roles" && c.Value == "user-role");
         var adminRole = context.User.FindFirst(c => c.Type == "roles" && c.Value == "admin-role");
+
+        if (userRole == null)
+        {
+            userRole = context.User.FindFirst(c => c.Type == msMagicNamespace && c.Value == "user-role");
+        }
+
+        if (adminRole == null)
+        {
+            userRole = context.User.FindFirst(c => c.Type == msMagicNamespace && c.Value == "admin-role");
+        }
 
         if (userRole != null || adminRole != null)
         {
